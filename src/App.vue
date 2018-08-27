08c9907d97d12a038f299fee662a263dff1cc483
yhlben <yinhengliben@gmail.com>
2018-08-27 22:29:06 +0800

    vscode config

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-    <div id="body">
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>
-          <button>123123</button>
-        </el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
-    </div>
+	<div id="body">
+		<el-container>
+			<el-header>Header</el-header>
+			<el-main>
+				<el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+					<el-select v-model="select" slot="prepend" placeholder="请选择">
+						<el-option label="餐厅名" value="1"></el-option>
+						<el-option label="订单号" value="2"></el-option>
+						<el-option label="用户电话" value="3"></el-option>
+					</el-select>
+					<el-button slot="append" icon="el-icon-search"></el-button>
+				</el-input>
+			</el-main>
+			<el-footer>Footer</el-footer>
+		</el-container>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			input5: '1231',
+			select: ''
+		};
 	},
-	methods: {
-	}
+	methods: {}
 };
 </script>
 

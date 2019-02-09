3e4c241f46277ac77c69b3f7c0053fcb0764ff1f
yhlben <yinhengliben@gmail.com>
2019-02-09 10:22:51 +0800

    test: 增加测试覆盖率

--- test/unit/specs/ScrollToTop.spec.js
+++ test/unit/specs/ScrollToTop.spec.js
@@ -1,8 +1,17 @@
 import { expect } from 'chai';
 import ScrollToTop from '~/components/ScrollToTop.vue';
+import { mount } from '@vue/test-utils';
+
+const wrapper = mount(ScrollToTop, {
+  propsData: { scrollElement: document.body },
+});
 
 describe('ScrollToTop.vue', () => {
   it('存在ScrollToTop组件。', () => {
     expect(ScrollToTop).to.exist;
+  });
+
+  it('是否渲染成功', () => {
+    expect(wrapper.find('#scroll-to-top')).to.exist;
   });
 });
